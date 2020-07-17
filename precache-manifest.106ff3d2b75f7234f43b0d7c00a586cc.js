43e2bbb28b5cd958e7678286efcde3d7933f93ef
Daniil <dan98l@mail.ru>
2020-07-18 00:58:31 +0700

    Updates

---
+++ precache-manifest.106ff3d2b75f7234f43b0d7c00a586cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5df6539a2b3794bfc4b4284deacc450",
+    "url": "/formular-app/index.html"
+  },
+  {
+    "revision": "0407bad4d60c894c636a",
+    "url": "/formular-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9122f3c2abb4e23939a8",
+    "url": "/formular-app/static/js/2.6486c077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/formular-app/static/js/2.6486c077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0407bad4d60c894c636a",
+    "url": "/formular-app/static/js/main.bf44cb81.chunk.js"
+  },
+  {
+    "revision": "8137ad630ecc8a1059d2",
+    "url": "/formular-app/static/js/runtime-main.5736f3b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/formular-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
