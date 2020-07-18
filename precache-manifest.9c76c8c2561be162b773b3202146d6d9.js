b0532328b6c3e8cc7494417a5a2c3248a1b8ef04
Daniil <dan98l@mail.ru>
2020-07-18 13:54:31 +0700

    Updates

--- precache-manifest.9c76c8c2561be162b773b3202146d6d9.js
+++ precache-manifest.9c76c8c2561be162b773b3202146d6d9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8f7efacf890b5a7794af1e0e9ea4f806",
+    "revision": "a7e52c9e551c0512c155c2d361015e50",
     "url": "/formular-app/index.html"
   },
   {
-    "revision": "353555aafdd95f43d442",
+    "revision": "b4d76a671d859fe2fb98",
     "url": "/formular-app/static/css/main.1593af7c.chunk.css"
   },
   {
-    "revision": "d33f3fee771a7e40439f",
-    "url": "/formular-app/static/js/2.9b74941b.chunk.js"
+    "revision": "b805668ff13a29c00d6d",
+    "url": "/formular-app/static/js/2.301e722b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/formular-app/static/js/2.9b74941b.chunk.js.LICENSE.txt"
+    "url": "/formular-app/static/js/2.301e722b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "353555aafdd95f43d442",
-    "url": "/formular-app/static/js/main.8dbb3359.chunk.js"
+    "revision": "b4d76a671d859fe2fb98",
+    "url": "/formular-app/static/js/main.5d5d72d0.chunk.js"
   },
   {
     "revision": "8137ad630ecc8a1059d2",
