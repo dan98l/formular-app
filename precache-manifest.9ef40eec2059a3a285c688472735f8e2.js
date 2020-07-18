b549c3206fd9d6d3ddda11ae7b705ec3b296bda3
Daniil <dan98l@mail.ru>
2020-07-18 14:01:15 +0700

    Updates

---
+++ precache-manifest.9ef40eec2059a3a285c688472735f8e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1924b73c3cbf3e19315c5b7d4267c7a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0c3548e17934cc08dbc",
+    "url": "./static/css/main.1593af7c.chunk.css"
+  },
+  {
+    "revision": "b805668ff13a29c00d6d",
+    "url": "./static/js/2.301e722b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.301e722b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c3548e17934cc08dbc",
+    "url": "./static/js/main.27712931.chunk.js"
+  },
+  {
+    "revision": "213de2d36447bc152e94",
+    "url": "./static/js/runtime-main.0ed2c788.js"
+  }
+]);
